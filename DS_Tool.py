c5954002962f1cba99ee5f1f0cd7eaf19589bab3
Goral Patel <105250604+GxPatel@users.noreply.github.com>
2024-08-28 13:47:30 -0400

    Update DS_Tool.py

--- DS_Tool.py
+++ DS_Tool.py
@@ -116,7 +116,7 @@ st.markdown("""
         left: 0;
         bottom: 0;
         width: 100%;
-        background-color: rgba(255,255,255,0);
+        background-color: rgba(255,255,255,0.3);
         text-align: center;
         font-size: 12px;
         color: black;
